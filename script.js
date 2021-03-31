b1dad15d8f8308e4e150bb9b31a7e1db258b063e
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2021-03-31 14:20:31 -0500

    completed assignment

--- script.js
+++ script.js
@@ -5,20 +5,26 @@ const MAX_DURATION = 3000;
 const snowflakesContainer = document.getElementById("snowflakes-container");
 
 
-// color
+// color in create snow flake function
 const colorInput = document.querySelector("input[name='color']");
 let color = "#ffffff";
 
 
 // rain or snow
 const snowVsRainrInput = document.querySelector("input[name='snow-vs-rain']")
+function randomIcon() {
+    if (Math.random() < Number(snowVsRainrInput.value)) {
+      return "fa-tint";
+    } else {
+      return "fa-snowflake";
+    }
+}
 
 
 // quantity
-let quantityAmount =400;
+let quantityAmount =200;
 const quantityInput = document.getElementById("quantity");
 let time = setInterval(() => createSnowflake(), quantityAmount);
-
 quantityInput.addEventListener("input", ()=>{
   switch(quantityInput.value) {
   case "0":
@@ -41,22 +47,15 @@ clearInterval(time);
 time=setInterval(() => createSnowflake(), quantityAmount);
 });
 
-// wind 
+// wind
 const windInput = document.getElementById("wind");
-
 
 
 function randint(lo, hi) {
   return Math.random() * (hi - lo) + lo;
 }
 
-function randomIcon() {
-    if (Math.random() < Number(snowVsRainrInput.value)) {
-      return "fa-tint";
-    } else {
-      return "fa-snowflake";
-    }
-}
+
 function createSnowflake() {
   const snowFlake = document.createElement("i");
   snowFlake.classList.add("fas", randomIcon());
@@ -64,37 +63,15 @@ function createSnowflake() {
   snowFlake.style.opacity = Math.random();
   snowFlake.style.fontSize = randint(MIN_SIZE, MAX_SIZE) + "px";
   snowFlake.style.color = color;
+
   colorInput.addEventListener("change", () => {
     color = colorInput.value;
   })
-
-  windInput.addEventListener("change", () => {
-    switch(windInput.value) {
-    case "-40":
-      snowFlake.style.transform= "translateX(" + (-500) + "px) ";
-      break;
-    case "-20":
-      snowFlake.style.transform= "translateX(" + (-300) + "px) ";
-      break;
-    case "-10":
-      snowFlake.style.transform= "translateX(" + (-100) + "px) ";
-      break;
-    case "10":
-      snowFlake.style.transform= "translateX(" + (100) + "px) ";
-      break;
-    case "20":
-      snowFlake.style.transform= "translateX(" + (300) + "px) ";
-      break;
-    case "40":
-      snowFlake.style.transform= "translateX(" + (500) + "px) ";
-      break;
-    }
-    })
   snowflakesContainer.appendChild(snowFlake);
 
   snowFlake
     .animate(
-      { transform: `translate(0vw, 100vh)` },
+      { transform: `translate(${windInput.value}vw, 100vh)` },
       { duration: randint(MIN_DURATION, MAX_DURATION) }
   )
   
